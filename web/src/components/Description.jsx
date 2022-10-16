7b88c795f252d16dc8c77be018e1cfe07f610bab
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2022-10-15 23:30:03 -0400

    Fix CSS

--- web/src/components/Description.jsx
+++ web/src/components/Description.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 
 class Description extends Component {
-    render() {
-        return (
-            <div className="description bg-primary">
-                <div>
-                    <h3 className="subtitle">{this.props.sub}</h3>
-                    <ul className="list">
-                        <li>{this.props.l1}</li>
-                        <li>{this.props.l2}</li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="description">
+        <div className="descriptionBody">
+          <h3 className="subtitle">{this.props.sub}</h3>
+          <ul className="list">
+            <li className="list">{this.props.l1}</li>
+            <li className="list">{this.props.l2}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Description;
