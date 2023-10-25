80ac5eb11db97aa7357b85e8db2801cad6bcdf85
petersonguo <petersonguo@gmail.com>
2023-10-25 18:27:59 -0400

    Fix Timer, autocorrect, speech

--- src/components/Cam.jsx
+++ src/components/Cam.jsx
@@ -1,7 +1,8 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Cam.css';
 import Webcam from "react-webcam";
 import Tesseract from "tesseract.js";
+import {GoMute, GoUnmute} from "react-icons/go";
 
 const videoConstraints = {
     width: 440,
@@ -12,53 +13,58 @@ const videoConstraints = {
 export default function Cam() {
     const webcamRef = useRef(null);
     const [text, setText] = useState("");
-    const [toggle, setToggle] = useState(true);
+    const [speak, setSpeak] = useState(false);
+    const [readData, setReadData] = useState(false);
 
-    const autoCorrect = useCallback(() => {
-        fetch("/autocorrect", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text: text
-            })
-        })
-            .then(res => res.json())
-            .then(response => {
-                setText(response.text);
-            });
-    }, [text]);
-
-    const OCR = useCallback(() => {
-        Tesseract.recognize(
-            webcamRef.current.getScreenshot(), 'eng'
-        )
-            .catch(x => {
-                console.error(x);
-            })
-            .then(result => {
-                let txt = result.data.text;
-                console.log(txt + ": " + result.data.confidence);
-                if (result.data.confidence > 50) {
-                    autoCorrect();
-                    console.log('autocorrected: ' + text);
-                }
-                console.log('speech: ' + text);
-                if (!toggle) {
-                    const msg = new SpeechSynthesisUtterance();
-                    msg.txt = text;
-                    window.speechSynthesis.speak(msg);
-                }
-            });
-    }, [autoCorrect, text, toggle]);
+    async function talk(text) {
+        const speech = new SpeechSynthesisUtterance();
+        speech.txt = text;
+        window.speechSynthesis.speak(speech);
+    }
 
     useEffect(() => {
-        setInterval(() => OCR, 10);
-    }, [OCR]);
+        const pause = setInterval(async () => {
+            if (readData) {
+                console.log('run')
+                await Tesseract.recognize(
+                    webcamRef.current.getScreenshot(), 'eng'
+                )
+                    .catch(x => {
+                        console.error(x);
+                    })
+                    .then(async result => {
+                        let txt = result.data.text;
+                        console.log(txt + ": " + result.data.confidence);
+                        if (result.data.confidence > 50) {
+                            await fetch("/autocorrect", {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    text: txt
+                                })
+                            })
+                                .then(res => res.json())
+                                .then(response => {
+                                    txt = response.text;
+                                    setText(response.text);
+                                });
+                            console.log('autocorrected: ' + text);
+                            if (speak) {
+                                console.log('speech: ' + text);
+                                await talk(text);
+                            }
+                        }
+                    });
+            }
+        }, 500);
+        return () => clearInterval(pause);
+    }, [text, readData, speak]);
 
     return (
         <div className={"my-[40px] justify-center items-center flex"}>
+
             <main className="items-center">
                 <Webcam
                     audio={false}
@@ -69,11 +75,21 @@ export default function Cam() {
                 />
                 <h1 className={"font-[30px] mt-[15px]"}>Extracted Text</h1>
                 <p className="text-box extractedText flex justify-center"> {text} </p>
-                <button onClick={() => {
-                    setToggle(!toggle)
-                }} className={"h-[50] m-5"}>
-                    {toggle ? 'Start Reading' : 'Stop Reading'}
-                </button>
+                <div className={"flex row"}>
+                    <button onClick={() => {
+                        setReadData(!readData)
+                    }} className={"h-[50] m-5"}>
+                        {!readData ? 'Start Reading' : 'Stop Reading'}
+                    </button>
+                    {
+                        speak ?
+                            <GoUnmute className={"text-white hover:cursor-pointer"}
+                                      size={30}
+                                      onClick={() => setSpeak(!speak)}/> :
+                            <GoMute className={"text-white hover:cursor-pointer"} size={30}
+                                    onClick={() => setSpeak(!speak)}/>
+                    }
+                </div>
             </main>
         </div>
     );
